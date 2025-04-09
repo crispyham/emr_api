ed22f0ca6d3412bb0d385ff57c071e2f25a9be12
Chris Pham <chpham@mail.fhsu.edu>
2025-04-09 00:50:08 -0500

    update Database.php

--- config/Database.php
+++ config/Database.php
@@ -70,36 +70,3 @@ class Database {
         return $this->conn ?? $this->connect();
     }
 }
-// end prodution php
-
-/*
-// local php
-class Database {
-    private $conn;
-
-    private $host = "localhost";
-    private $port = "5432";
-    private $db_name = "emr_platform";
-    private $username = "postgres";
-    private $password = "default"; // ← Replace with your actual DB password
-
-    public function connect() {
-        $this->conn = null;
-
-        try {
-            $this->conn = new PDO(
-                "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name}",
-                $this->username,
-                $this->password
-            );
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch(PDOException $e) {
-            echo json_encode(['message' => 'Database Connection Error']);
-            exit();
-        }
-
-        return $this->conn;
-    }
-}
-// end local php
-*/
